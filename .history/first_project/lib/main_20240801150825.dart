a18bd6a79c83d878e599b6fe23725671a9314226
iamfyke <fyketonel@gmail.com>
2024-08-06 15:01:12 +0800

    first commit

--- .history/first_project/lib/main_20240801150825.dart
+++ .history/first_project/lib/main_20240801150825.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
