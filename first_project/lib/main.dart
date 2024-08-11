4768b2bb4a95a8012b22e413b5b14aa70a4d08c0
iamfyke <fyketonel@gmail.com>
2024-08-11 15:31:16 +0800

    changed string fonts

--- first_project/lib/main.dart
+++ first_project/lib/main.dart
@@ -2,45 +2,37 @@ import 'package:flutter/material.dart';
 
 int number = 0;
 
-void main() => runApp(MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Cebuano++',
-          style: TextStyle(
-            fontFamily: 'ProductSans',
-          ),
-        ),
-        centerTitle: true,
-        backgroundColor: const Color.fromARGB(255, 240, 201, 84),
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Cebuano++',
+      theme: ThemeData(
+        fontFamily: 'ProductSans',
       ),
-      body: Center(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Cebuano++'),
+          centerTitle: true,
+          backgroundColor: const Color.fromARGB(255, 240, 201, 84),
+        ),
+        body: Center(
           child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            "Hello mga real niggas!",
-            style: TextStyle(
-              letterSpacing: 1.0,
-              color: Colors.grey[600],
-              fontFamily: 'ProductSans',
-            ),
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Hello mga real niggas!',
+                style: TextStyle(
+                  letterSpacing: 1.0,
+
+                )
+              )
+            ],
           ),
-          Text(
-            number.toString(),
-            style: TextStyle(
-              fontSize: 70,
-              fontFamily: 'ProductSans',
-            ),
-          ),
-        ],
-      )),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        backgroundColor: const Color.fromARGB(255, 240, 201, 84),
-        child: Text(
-          '+',
-          style: TextStyle(fontSize: 30),
         ),
       ),
-    )));
+    );
+  }
+}
