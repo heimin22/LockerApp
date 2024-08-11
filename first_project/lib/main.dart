03fd7e9e7c459ccfd97f9efe0fa67610c747db4d
iamfyke <fyketonel@gmail.com>
2024-08-11 15:41:44 +0800

    recoding

--- first_project/lib/main.dart
+++ first_project/lib/main.dart
@@ -26,11 +26,24 @@ class MyApp extends StatelessWidget {
                 'Hello mga real niggas!',
                 style: TextStyle(
                   letterSpacing: 1.0,
-                  color: Colors.grey[100],
+                  color: Colors.grey[600],
                 ),
               ),
-
+              Text(
+                number.toString(),
+                style: TextStyle(
+                  fontSize: 70,
+                ),
+              ),
             ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          backgroundColor: const Color.fromARGB(255, 240, 201, 84),
+          child: Text(
+            '+',
+            style: TextStyle(fontSize: 30),
           ),
         ),
       ),
