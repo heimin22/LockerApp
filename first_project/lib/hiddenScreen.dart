d33352ed928cd6bc6d8d5e8c7dc092c681427c47
iamfyke <fyketonel@gmail.com>
2024-08-14 16:49:24 +0800

    the hidden screen button is now working perfectly

--- first_project/lib/hiddenScreen.dart
+++ first_project/lib/hiddenScreen.dart
@@ -1,1 +1,21 @@
 import 'package:flutter/material.dart';
+
+class HiddenScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hidden Screen'),
+        backgroundColor: const Color.fromARGB(255, 240, 201, 84),
+      ),
+      body: Center(
+        child: Text (
+          "This is the hidden screen.",
+          style: TextStyle(
+            fontSize: 24,
+          ),
+        )
+      )
+    );
+  }
+}
