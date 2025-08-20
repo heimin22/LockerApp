0cc784644027ffe6ddd8353687c2e2901d6bd453
heimin22 <fyketonel@gmail.com>
2025-08-21 02:05:44 +0800

    Remove all project files and configurations for the Flutter application, including Android, iOS, Linux, macOS, and web directories, effectively resetting the project.

---
+++ Locker/lib/utils/toast_utils.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+class ToastUtils {
+  static const Color _successBg = Color(0xFF00CC58);
+  static const Color _errorBg = Color(0xFFE53E3E);
+  static const Color _warningBg = Color(0xFFF59E0B);
+  static const Color _infoBg = Color(0xFFF5F5F5);
+  static const Color _lightText = Color(0xFFF5F5F5);
+  static const Color _darkText = Color(0xFF121212);
+
+  
+}
